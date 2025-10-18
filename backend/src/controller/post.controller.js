06c94cb77c773d11d1b2fd29786062207ecf4627
Rajeevanand07 <anandrajeev1234@gmail.com>
2025-10-18 23:15:54 +0530

    integrated AI

--- backend/src/controller/post.controller.js
+++ backend/src/controller/post.controller.js
@@ -1,9 +1,15 @@
+const { generateCaption } = require("../service/ai.service");
+
 async function createPost(req, res) {
-  const image = req.file;
-  res.status(201).json({ message: "Post created successfully"});
-  
+  const file = req.file;
+  const base64Image = new Buffer.from(file.buffer).toString("base64");
+  const caption = await generateCaption(base64Image);
+
+  res
+    .status(201)
+    .json({ message: "Post created successfully", caption: caption });
 }
 
 module.exports = {
-  createPost
+  createPost,
 };
--- backend/src/service/ai.service.js
+++ backend/src/service/ai.service.js
@@ -1,16 +1,31 @@
-import { GoogleGenAI } from "@google/genai";
+const { GoogleGenAI } = require("@google/genai");
 
 // The client gets the API key from the environment variable `GEMINI_API_KEY`.
 const ai = new GoogleGenAI({
-  apiKey: 'AIzaSyCR8UCZnpbNle1GjglIxYysBYqupewN65c',
+  apiKey: "AIzaSyCR8UCZnpbNle1GjglIxYysBYqupewN65c",
 });
 
-async function main() {
+async function generateCaption(base64ImageFile) {
+  const contents = [
+    {
+      inlineData: {
+        mimeType: "image/jpeg",
+        data: base64ImageFile,
+      },
+    },
+    { text: "Caption this image." },
+  ];
   const response = await ai.models.generateContent({
     model: "gemini-2.5-flash",
-    contents: "Explain how AI works in a few words",
+    contents: contents,
+    config: {
+      systemInstruction:
+        "You are an AI that generates short, meaningful, and engaging captions for images. Each caption should be between 10 and 15 words. It should capture the emotion, vibe, or story behind the image in a natural, smooth, and creative way. Avoid over-describing, complex words, or filler phrases. Do not add hashtags or explanations. You may use a single emoji if it enhances the vibe.",
+    },
   });
-  console.log(response.text);
+  return response.text;
 }
 
-main();
+module.exports = {
+  generateCaption,
+};
