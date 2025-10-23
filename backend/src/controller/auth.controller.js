406d03dce2d321c547f239db395ebd82aed160dc
Rajeevanand07 <anandrajeev1234@gmail.com>
2025-10-23 13:32:35 +0530

    added logs in backend

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -89,6 +89,8 @@ async function logoutUser(req, res) {
   
   res.clearCookie("token",{
     httpOnly: true,
+        secure:true,
+    sameSite:"none"
   });
   res.status(200).json({ message: "Logout successful" });
 }
